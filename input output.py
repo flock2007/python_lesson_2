b4647a715cad5276333f55e7ec054e7c4bc4b8c8
flock2007 <flock2007@yandex.ru>
2023-02-10 02:26:10 +0300

    done

---
+++ input output.py
@@ -0,0 +1,8 @@
+# predl = 'Привет, как твои дела?'
+# predl = str(predl)
+# abc = len(predl)
+# index = predl[12:abc]
+# print(index)
+list1 = [3, 2, 8, 5, 10, 6]
+max_number = max(list1)
+print("Наибольшее число:", max_number)
